d28540166f9d26ad0c9ec83bc2d2ebaedbb15998
Camilla Aoki <caokicontato@gmail.com>
2024-02-19 20:44:44 -0300

    about html parsing

--- Ch5 - Internet Data/htmlparsing_start.py
+++ Ch5 - Internet Data/htmlparsing_start.py
@@ -5,15 +5,35 @@
 
 from html.parser import HTMLParser
 
+paragraph = 0
+
 class MyHTMLParser(HTMLParser):
     def handle_comment(self, data):
-        pass
+        print("Encounter a comment", data)
+        pos = self.getpos()
+        print("At line", pos[0], "position", pos[1])
 
     def handle_starttag(self, tag, attrs):
-        pass
+        print("Encounter a start tag", tag)
+        pos = self.getpos()
+        print("At line", pos[0], "position", pos[1])
+
+        global paragraph
+        if tag == "p":
+            paragraph += 1
+        
+        if len(attrs) > 0:
+            print("Attributes")
+            for a in attrs:
+                print("\t", a[0], "=", a[1])
+
 
     def handle_data(self, data):
-        pass
+        print("Encounter text data", data)
+        if data .isspace():
+            return
+        pos = self.getpos()
+        print("At line", pos[0], "position", pos[1])        
 
 def main():
     # instantiate the parser and feed it some HTML
@@ -23,6 +43,7 @@ def main():
     if f.mode == "r":
         contents = f.read() # read the entire file
         parser.feed(contents)    
+    print("Paragraph tags", paragraph)
 
 if __name__ == "__main__":
     main()
